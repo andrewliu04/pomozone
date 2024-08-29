be14dd2a8bb84ed86e8b7bd407b3a4cb08ec4aba
andrewliu04 <andrewliu245@gmail.com>
2024-08-29 01:39:31 -0500

    Connect to MongoDB

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config(); // .env variables
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+const dbURI = process.env.MONGO_URI || 'mongodb://localhost:27017/pomodoro';
+mongoose.connect(dbURI)
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
